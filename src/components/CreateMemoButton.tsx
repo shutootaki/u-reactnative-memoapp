19655328c5fa8dd71b9600190d71fe44225f62e8
shutootaki <ootaki200109@gmail.com>
2022-12-16 07:42:56 +0900

    chore: button styled

--- src/components/CreateMemoButton.tsx
+++ src/components/CreateMemoButton.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export const CreateMemoButton = () => {
   return (
-    <View>
-      <Text>+</Text>
+    <View style={styels.createMemoButton}>
+      <Text style={styels.createMemoButtonText}>+</Text>
     </View>
   );
 };
+
+const styels = StyleSheet.create({
+  createMemoButton: {
+    backgroundColor: '#467FD3',
+    alignItems: 'center',
+    width: 64,
+    height: 64,
+    borderRadius: 32,
+    position: 'absolute',
+    right: 40,
+    bottom: 40,
+    shadowColor: '#0000000',
+    shadowOffset: { width: 0, height: 8 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    elevation: 8
+  },
+  createMemoButtonText: {
+    color: '#ffffff',
+    fontSize: 40,
+    marginTop: 4
+  }
+});
