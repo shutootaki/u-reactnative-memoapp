971a1c275183cb6fa5526bb2ad9c29bba2c196e5
shutootaki <ootaki200109@gmail.com>
2022-12-19 00:39:58 +0900

    feat: memo edit実装

--- src/components/templates/EditMemo.tsx
+++ src/components/templates/EditMemo.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { StyleSheet, TextInput } from 'react-native';
+import { MemoData } from '../../types/type';
 
-const EditMemo = () => {
+type Props = {
+  setEditMemoBody: React.Dispatch<React.SetStateAction<string>>;
+} & MemoData;
+
+const EditMemo: React.FC<Props> = ({ id, memoBody, setEditMemoBody }) => {
   return (
-    <TextInput style={styles.memoText} multiline>
-      意義のあるサービス開発に没頭する。 なぜなら、サービスは、社会的意義があるからこそ成り立つと考えるからです。
-      しかし、現状の技術力は他のエンジニアと比較して、低いと自覚おり、価値あるサービスをユーザーに届けられる状況ではありません。
-      なので、2〜3年を掛けて、技術力を磨き、その集大成として、社会的意義のあるサービス開発のコアメンバーとしてユーザーに価値を届けたいです。
+    <TextInput style={styles.memoText} multiline onChangeText={(text) => setEditMemoBody(text)}>
+      {memoBody}
     </TextInput>
   );
 };
--- src/screens/MemoEditScreen.tsx
+++ src/screens/MemoEditScreen.tsx
@@ -1,18 +1,47 @@
-import React from 'react';
-import { CircleButton } from '../components/atoms/CircleButton';
+import React, { useState } from 'react';
 import { KeyboardAvoidingView, StyleSheet, View } from 'react-native';
-import EditMemo from '../components/templates/EditMemo';
-import { TNav } from './Login';
+import { RouteProp } from '@react-navigation/native';
+import firebase from 'firebase';
+import { StackNavigationProp } from '@react-navigation/stack';
 
-const MemoEditScreen: React.FC<TNav> = ({ navigation }) => {
-  const onPress = () => {
-    navigation.goBack();
+import EditMemo from '../components/templates/EditMemo';
+import { CircleButton } from '../components/atoms/CircleButton';
+import { RootStackParamList } from '../types/type';
+
+type MemoEditScreenNavigationProp = StackNavigationProp<RootStackParamList, 'MemoEdit'>;
+type MemoEditScreenRouteProp = RouteProp<RootStackParamList, 'MemoEdit'>;
+type Props = {
+  navigation: MemoEditScreenNavigationProp;
+  route: MemoEditScreenRouteProp;
+};
+
+const MemoEditScreen: React.FC<Props> = ({ navigation, route }) => {
+  const { id, memoBody } = route.params;
+  const [editMemoBody, setEditMemoBody] = useState(memoBody);
+  const db = firebase.firestore();
+  const { currentUser } = firebase.auth();
+
+  const updateMemoHandler = () => {
+    currentUser &&
+      db
+        .collection(`users/${currentUser.uid}/memos`)
+        .doc(id)
+        .set({
+          memoBody: editMemoBody,
+          updatedAt: new Date()
+        })
+        .then(() => {
+          navigation.goBack();
+        })
+        .catch((error) => {
+          alert(`Error adding document: ${error}`);
+        });
   };
   return (
     <View style={styles.container}>
       <KeyboardAvoidingView style={styles.textEditBody} behavior="height">
-        <EditMemo />
-        <CircleButton iconName="check" onPress={onPress} />
+        <EditMemo id={id} memoBody={memoBody} setEditMemoBody={setEditMemoBody} />
+        <CircleButton iconName="check" onPress={updateMemoHandler} />
       </KeyboardAvoidingView>
     </View>
   );
